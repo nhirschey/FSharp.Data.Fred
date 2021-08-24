ba6cafb34e4fa1ec9be2f3b8926c71dda58ad2d9
Nicholas Hirschey <nhirschey@users.noreply.github.com>
2021-08-24 13:20:38 +0100

    fake build script.

--- build.fsx
+++ build.fsx
@@ -1,36 +1,128 @@
-// --------------------------------------------------------------------------------------
-// FAKE build script
-// --------------------------------------------------------------------------------------
-#r "paket: groupref build //"
+// This is a FAKE 5.0 script, run using
+//    dotnet fake build
+
+#r "paket: groupref fake //"
+
+#if !FAKE
 #load ".fake/build.fsx/intellisense.fsx"
+#r "netstandard"
+#endif
 
+open System
+open System.Xml.Linq
 open Fake.Core
-open Fake.DotNet
-open Fake.Tools
-open Fake.IO
-open Fake.IO.FileSystemOperators
-open Fake.IO.Globbing.Operators
 open Fake.Core.TargetOperators
-open Fake.Api
+open Fake.IO.Globbing.Operators
+open Fake.IO.FileSystemOperators
+open Fake.DotNet
+open Fake.IO
+open Fake.Tools
 
+Environment.CurrentDirectory <- __SOURCE_DIRECTORY__
 
-Target.initEnvironment ()
+// Information about the project to be used at NuGet and in AssemblyInfo files
+let project = "FSharp.Data.Fred"
 
-Target.create "Clean" (fun _ ->
-    !! "src/**/bin"
-    ++ "src/**/obj"
-    |> Shell.cleanDirs 
+let summary = "A wrapper around the Fedederal Reserve Economic Data (FRED) API."
+
+let license = "MIT License"
+
+let configuration = DotNet.BuildConfiguration.fromEnvironVarOrDefault "configuration" DotNet.BuildConfiguration.Release
+
+// Folder to deposit deploy artifacts
+let artifactsDir = __SOURCE_DIRECTORY__ @@ "artifacts"
+
+// Read release notes document
+let release = ReleaseNotes.load "RELEASE_NOTES.md"
+
+let solutionFile = "FSharp.Data.Fred.sln"
+
+// --------------------------------------------------------------------------------------
+// Generate assembly info files with the right version & up-to-date information
+
+Target.create "AssemblyInfo" (fun _ ->
+    let info = [
+        AssemblyInfo.Product project
+        AssemblyInfo.Description summary
+        AssemblyInfo.Version release.AssemblyVersion
+        AssemblyInfo.FileVersion release.AssemblyVersion
+        AssemblyInfo.InformationalVersion release.NugetVersion
+        AssemblyInfo.Copyright license
+    ]
+
+    AssemblyInfoFile.createFSharp $"src/{project}/AssemblyInfo.fs" info
+    let versionProps =
+        XElement(XName.Get "Project",
+            XElement(XName.Get "PropertyGroup",
+                XElement(XName.Get "Version", release.NugetVersion),
+                XElement(XName.Get "PackageReleaseNotes", String.toLines release.Notes)
+            )
+        )
+    versionProps.Save("version.props")
 )
 
+// Clean build results
+// --------------------------------------------------------------------------------------
+
+Target.create "Clean" (fun _ ->
+    !! artifactsDir
+    ++ "temp"
+    |> Shell.cleanDirs
+    // in case the above pattern is empty as it only matches existing stuff
+    ["bin"; "temp"; "tests/bin"]
+    |> Seq.iter Directory.ensure
+)
+
+// Build library
+// --------------------------------------------------------------------------------------
 Target.create "Build" (fun _ ->
-    !! "src/**/*.*proj"
-    |> Seq.iter (DotNet.build (fun o -> { o with Configuration = DotNet.BuildConfiguration.Debug }))
+    solutionFile
+    |> DotNet.build (fun opts -> { opts with Configuration = configuration } )
+)
+
+Target.create "Tests" (fun _ ->
+    solutionFile
+    |> DotNet.test (fun opts ->
+        { opts with
+            Blame = true
+            NoBuild = true
+            Framework = Some "net5.0"
+            Configuration = configuration
+            ResultsDirectory = Some "TestResults"
+            Logger = Some "trx"
+        })
+)
+
+// --------------------------------------------------------------------------------------
+// Build a NuGet package
+
+Target.create "NuGet" (fun _ ->
+    DotNet.pack (fun pack ->
+        { pack with
+            OutputPath = Some artifactsDir
+            Configuration = configuration
+        }) solutionFile
+)
+
+// Generate the documentation by dogfooding the tools pacakge
+// --------------------------------------------------------------------------------------
+
+Target.create "GenerateDocs" (fun _ ->
+    Shell.cleanDir ".fsdocs"
+    DotNet.exec id "fsdocs" "build --strict --clean --properties Configuration=Release" |> ignore
 )
 
 Target.create "All" ignore
 
+// clean and recreate assembly information on release
 "Clean"
+  ==> "AssemblyInfo"
   ==> "Build"
+  ==> "NuGet"
+  ==> "Tests"
+  ==> "GenerateDocs"
   ==> "All"
 
 Target.runOrDefault "All"
+
+
--- src/FSharp.Data.Fred/Library.fs
+++ src/FSharp.Data.Fred/Library.fs
@@ -6,20 +6,13 @@ open FSharp.Data
 /// <summary>
 ///   A module
 /// </summary>
-///
-/// <namespacedoc>
-///   <summary>A namespace to remember</summary>
-///
-///   <remarks>More on that</remarks>
-/// </namespacedoc>
-///
 [<RequireQualifiedAccess>]
 module Fred =
     type Series = CsvProvider<Sample="Date (date),Value (float)",MissingValues=".">
     let private fredUrl series = $"https://fred.stlouisfed.org/graph/fredgraph.csv?id={series}"
     
     ///<summary>Loads a FRED data series as a <c>FSharp.Data.CsvFile</c></summary>
-    /// <param name="series">The series name such as GS10, EXUSEU, etc.</param>
+    ///<param name="series">The series name such as GS10, EXUSEU, etc.</param>
     let Load (series:string) =  
         // I'm using "Load" instead of "load" to mimic CsvFile.Load
         Series.Load(fredUrl series)
